e4c180d2c875c312aeabea26344ccf1520e3e466
Danny Willett <dannywill8@gmail.com>
2021-04-02 18:22:37 -0400

    Fixed what Nelson did from Update 3.21.13.0.

--- DiscordCommunicator/DiscordInterface.cs
+++ DiscordCommunicator/DiscordInterface.cs
@@ -6,25 +6,60 @@ using static Rocket.Core.Logging.Logger;
 using System.IO.MemoryMappedFiles;
 using SDG.Unturned;
 using CommsLib;
+using MySql.Data.MySqlClient;
+using Rocket.Unturned.Player;
 
 namespace DiscordCommunicator
 {
     public class DiscordInterface : RocketPlugin<DiscordInterfaceConfig>
     {
-        private IEnumerator timeout;
-        private int timeoutCounter = 0;
         private IEnumerator coroutine;
+        private MySqlConnection SQL;
+        private MMFInterface _interface;
         protected override void Load()
         {
+            _interface = new MMFInterface(Configuration.Instance.MaxPlayerCount, Configuration.Instance.MaxNameLength);
             coroutine = WaitAndPrint(Configuration.Instance.RefreshRateSeconds);
             StartCoroutine(coroutine);
             Log("Started loop at game time: " + Time.time + " seconds. " +
-                "Will refresh every " + Configuration.Instance.RefreshRateSeconds.ToString() + " seconds."
+                "Will refresh every " + Configuration.Instance.RefreshRateSeconds.ToString() + " seconds." + 
+                (Configuration.Instance.LogWhenSendData ? " A log will be added everytime the playerlist is sent." : "")
             );
+            if(Configuration.Instance.RemoveRankPrefix)
+            {
+                try
+                {
+                    SQL = new MySqlConnection(Configuration.Instance.SQLLogin.ConnectionString);
+                }
+                catch (MySqlException ex)
+                {
+                    Log("Couldn't connect to MySQL. Error:\n" + ex.ToString());
+                }
+                try
+                {
+                    Connect();
+                }
+                catch (MySqlException ex)
+                {
+                    Log(ex.ToString());
+                }
+            }
         }
         protected override void Unload()
         {
-            SendData(new FPlayerList(MMFInterface.ExtendArray(new string[] { "OFFLINE", "INTENTIONAL" }, MMFInterface.MaxPlayerCount), DateTime.Now));
+            SendData(new FPlayerList(MMFInterface.ExtendArray(new string[] { "OFFLINE", "INTENTIONAL" }, _interface.MaxPlayerCount), new byte[_interface.MaxPlayerCount], DateTime.Now));
+            if(Configuration.Instance.RemoveRankPrefix)
+            {
+                try
+                {
+                    SQL.Close();
+                }
+                finally
+                {
+                    Log("Attempted to close MYSQL Connection");
+                }
+            }
+            StopCoroutine(coroutine);
             base.Unload();
         }
         private IEnumerator WaitAndPrint(float waitTime)
@@ -34,65 +69,92 @@ namespace DiscordCommunicator
             coroutine = WaitAndPrint(Configuration.Instance.RefreshRateSeconds);
             StartCoroutine(coroutine);
         }
-        private IEnumerator TimeoutWait(int WaitTime, int amount, MemoryMappedViewAccessor HandlerAccessor, MemoryMappedViewAccessor BotAccessor, byte[] send, MemoryMappedFile mmf, MemoryMappedFile handler)
-        {
-            yield return new WaitForSeconds(WaitTime / 1000f);
-            byte d = HandlerAccessor.ReadByte(0);
-            if (d != 1)
-            {
-                BotAccessor.WriteArray(0, send, 0, send.Length);
-                timeoutCounter++;
-                if (timeoutCounter > amount)
-                {
-                    timeoutCounter = 0;
-                    BotAccessor.Dispose();
-                    mmf.Dispose();
-                    HandlerAccessor.Dispose();
-                    handler.Dispose();
-                    StopCoroutine(timeout);
-                }
-                else
-                {
-                    timeout = TimeoutWait(WaitTime, amount, HandlerAccessor, BotAccessor, send, mmf, handler);
-                    StartCoroutine(timeout);
-                }
-            } else
-            {
-                timeoutCounter = 0;
-                BotAccessor.Dispose();
-                mmf.Dispose();
-                HandlerAccessor.Dispose();
-                handler.Dispose();
-                StopCoroutine(timeout);
-            }
-        }
         private void SendData(FPlayerList fPlayerList)
         {
-            MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen(this.Configuration.Instance.mmfName, MMFInterface.Length);
+            MemoryMappedFile mmf = MemoryMappedFile.CreateOrOpen(this.Configuration.Instance.mmfName, _interface.legacy ? _interface.LegacyLength : _interface.Length);
             MemoryMappedViewAccessor BotAccessor = mmf.CreateViewAccessor();
-            MemoryMappedFile handler = MemoryMappedFile.CreateOrOpen(this.Configuration.Instance.mmfName + "-handler", 1);
-            MemoryMappedViewAccessor HandlerAccessor = handler.CreateViewAccessor();
-            byte[] send = new byte[MMFInterface.MaxPlayerCount];
+            byte[] send;
             try
             {
-                send = MMFInterface.EncodeMessage(fPlayerList);
+                send = _interface.EncodeMessage(fPlayerList);
+                BotAccessor.WriteArray(0, send, 0, send.Length);
+                if (Configuration.Instance.LogWhenSendData)
+                    Log(send.Length.ToString() + " bytes of data sent over " + Configuration.Instance.mmfName);
             }
             catch (Exception ex)
             {
                 Log(ex.ToString());
+                Log("Data failed to send on " + Configuration.Instance.mmfName);
             }
-            BotAccessor.WriteArray(0, send, 0, send.Length);
-            timeout = TimeoutWait(100, 20, HandlerAccessor, BotAccessor, send, mmf, handler);
-            StartCoroutine(timeout);
         }
         private FPlayerList GetSendData()
         {
             string[] players = new string[Provider.clients.Count];
+            byte[] teams = new byte[_interface.MaxPlayerCount];
             for(int i = 0; i < players.Length; i++)
             {
-                players[i] = Provider.clients[i].playerID.nickName;
+                players[i] = Configuration.Instance.RemoveRankPrefix ? RemovePrefix(Provider.clients[i]) : Provider.clients[i].playerID.characterName;
+                teams[i] = (byte)(Provider.clients[i].player.quests.groupID.m_SteamID <= 255UL ? Provider.clients[i].player.quests.groupID.m_SteamID : 0);
             }
-            return new FPlayerList(MMFInterface.ExtendArray(players, MMFInterface.MaxPlayerCount), DateTime.Now);
+            return new FPlayerList(MMFInterface.ExtendArray(players, _interface.MaxPlayerCount), teams, DateTime.Now);
+        }
+        public static string RemoveOnce(string original, string toRemove)
+        {
+            int i = original.IndexOf(toRemove);
+            if (i == -1) return original;
+            else return original.Substring(0, i) + original.Substring(i + toRemove.Length);
+        }
+        public string RemovePrefix(SteamPlayer player)
+        {
+            string rtn;
+            UncreatedLib.XPLevel xp = new UncreatedLib.XPLevel((int)GetXP(player.playerID.steamID.m_SteamID, player.player.quests.groupID.m_SteamID));
+            if (player.player.quests.groupID.m_SteamID == 1UL)
+            {
+                rtn = RemoveOnce(player.playerID.nickName, $"[US-{xp.Abreviation}] ");
+            }
+            else if (player.player.quests.groupID.m_SteamID == 2UL)
+            {
+                rtn = RemoveOnce(player.playerID.nickName, $"[RU-{xp.Abreviation}] ");
+            }
+            else
+            {
+                rtn = RemoveOnce(player.playerID.nickName, $"[rec.] ");
+            }
+            return rtn;
+        }
+        public bool Connect()
+        {
+            try
+            {
+                SQL.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+                Log(ex.ToString());
+                return false;
+            }
+        }
+        public uint GetXP(ulong Steam64, ulong TeamID)
+        {
+            if (SQL.State != System.Data.ConnectionState.Open)
+                Connect();
+
+            uint xp = 0;
+            using (MySqlCommand Q = new MySqlCommand($"SELECT `Balance` FROM `xp` WHERE `Steam64`='{Steam64}' AND `Team`='{TeamID}';", SQL))
+            {
+                using (MySqlDataReader R = Q.ExecuteReader())
+                {
+                    while (R.Read())
+                    {
+                        xp = R.GetUInt32("Balance");
+                    }
+                    R.Close();
+                    R.Dispose();
+                    Q.Dispose();
+                }
+            }
+            return xp;
         }
     }
 }
--- DiscordCommunicator/DiscordInterfaceConfig.cs
+++ DiscordCommunicator/DiscordInterfaceConfig.cs
@@ -1,9 +1,5 @@
 ﻿using Rocket.API;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace DiscordCommunicator
 {
@@ -11,10 +7,39 @@ namespace DiscordCommunicator
     {
         public float RefreshRateSeconds;
         public string mmfName;
+        [XmlElement("MySqlLoginData")]
+        public SqlData SQLLogin;
+        public bool RemoveRankPrefix;
+        public bool LogWhenSendData;
+        public int MaxPlayerCount;
+        public int MaxNameLength;
         public void LoadDefaults()
         {
             RefreshRateSeconds = 60.0f;
             mmfName = "semirp";
+            SQLLogin = new SqlData("localhost", "root", "password", "database", 3306);
+            RemoveRankPrefix = true;
+            LogWhenSendData = false;
+            MaxPlayerCount = 24;
+            MaxNameLength = 30;
+        }
+    }
+    public struct SqlData
+    {
+        public string IP;
+        public string Username;
+        public string Password;
+        public string Database;
+        public ushort Port;
+        public string ConnectionString { get { return $"server={IP};port={Port};uid={Username};pwd={Password};database={Database}"; } }
+
+        public SqlData(string IP, string Username, string Password, string Database, ushort Port)
+        {
+            this.IP = IP;
+            this.Username = Username;
+            this.Password = Password;
+            this.Database = Database;
+            this.Port = Port;
         }
     }
 }
